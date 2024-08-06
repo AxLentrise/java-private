ef9e8061fa2bb46b504d2ec3bcdbc01c4b84e146
Diogo Tiago Bertoldo <139913465+AxLentrise@users.noreply.github.com>
2024-08-06 16:23:48 -0300

    Updated TopPanel

--- chat-app-socket/src/com/client/entities/TopPanel.java
+++ chat-app-socket/src/com/client/entities/TopPanel.java
@@ -1,9 +1,6 @@
 package com.client.entities;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Toolkit;
+import java.awt.GridBagLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -13,68 +10,51 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import com.client.utils.Colors;
+import com.client.utils.GBC;
 
 public class TopPanel extends JPanel {
-
-	private static final int HEIGHT = 25;
-
+	
 	public JButton connect;
 	private JButton close;
 	private JButton minimize;
-
-	private JPanel midPanel;
 	private JLabel title;
 
 	public TopPanel(Runnable closeFrame, Runnable minimizeFrame) {
-		setLayout(new FlowLayout(FlowLayout.LEFT));
+		setLayout(new GridBagLayout());
 		setBackground(Colors.BG);
 		setForeground(Colors.FG);
 		setBorder(BorderFactory.createCompoundBorder( 	//
 			BorderFactory.createMatteBorder(0, 0, 1, 0, Colors.BG_LN), //
-			BorderFactory.createEmptyBorder(2, 5, 2, 5)	//
+			BorderFactory.createEmptyBorder(5, 5, 5, 5)	//
 		));
 
 		connect = new JButton("Login");
 		connect.setIcon(getIcon("/resources/user-24-edit.png"));
-		connect.setPreferredSize(new Dimension(75, HEIGHT));
 		connect.setBorder(new EmptyBorder(2, 5, 2, 5));
 		connect.setBackground(getBackground());
 		connect.setForeground(getForeground());
-		connect.addActionListener(e -> {
-		});
-
-		midPanel = new JPanel();
-		midPanel.setBackground(getBackground());
-		midPanel.setForeground(getForeground());
-		var screen = Toolkit.getDefaultToolkit().getScreenSize();
-		midPanel.setPreferredSize(new Dimension((int) screen.getWidth() / 2 - HEIGHT * 2, HEIGHT));
+		add(connect, new GBC(0, 0));
 
 		title = new JLabel("Application name - TODO");
-		title.setBorder(new EmptyBorder(2, 0, 0, 10));
 		title.setBackground(getBackground());
 		title.setForeground(getForeground());
-		midPanel.add(title, BorderLayout.NORTH);
-
-		close = new JButton();
-		close.setIcon(getIcon("/resources/close-16-edit.png"));
-		close.setPreferredSize(new Dimension(HEIGHT + 25, HEIGHT));
-		close.setBorder(new EmptyBorder(2, 5, 2, 5));
-		close.setBackground(getBackground());
-		close.setForeground(getForeground());
-		close.addActionListener(e -> closeFrame.run());
+		add(title, new GBC(1, 0).horizontal().left(7).right(7));
 
 		minimize = new JButton();
 		minimize.setIcon(getIcon("/resources/minus-24-edit.png"));
-		minimize.setPreferredSize(new Dimension(HEIGHT + 25, HEIGHT));
 		minimize.setBorder(new EmptyBorder(2, 5, 2, 5));
 		minimize.setBackground(getBackground());
 		minimize.setForeground(getForeground());
 		minimize.addActionListener(e -> minimizeFrame.run());
-
-		add(connect);
-		add(midPanel);
-		add(minimize);
-		add(close);
+		add(minimize, new GBC(2, 0));
+		
+		close = new JButton();
+		close.setIcon(getIcon("/resources/close-16-edit.png"));
+		close.setBorder(new EmptyBorder(2, 5, 2, 5));
+		close.setBackground(getBackground());
+		close.setForeground(getForeground());
+		close.addActionListener(e -> closeFrame.run());
+		add(close, new GBC(3, 0));
 	}
 
 	public final ImageIcon getIcon(String path) {
